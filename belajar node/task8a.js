23db47fb8ca7074799badd99a29656a18e3e9ff7
Windiana <krismanuyar@gmail.com>
2020-01-15 13:04:11 +0700

    Update task8a.js

--- belajar node/task8a.js
+++ belajar node/task8a.js
@@ -1,7 +1,8 @@
+// indentasi berantakan :D
 var hasil = function kuadrat(n,q){
     if(q==1){
     return n;
-}
+} //disini kalo mau dikasih else, atau {} nya dihilangkan sekalian
 {
     return n * kuadrat(n,q-1)
 }}
